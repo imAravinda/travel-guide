1d89aeff0a235973872164c0c5b45c0e0f266bc0
imAravinda <acfernando1999@gmail.com>
2022-08-31 08:23:50 +0530

    add preloader

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Destination from './Pages/Destination';
 import Blog from './Pages/Blog';
 import AboutUs from './Pages/AboutUs';
 import Aos from "aos";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "aos/dist/aos.css";
 function App() {
   const GlobalStyle = createGlobalStyle `
@@ -19,41 +19,55 @@ function App() {
       font-family: 'PT Sans', sans-serif;
     }
   `
+  const[loading,setLoading] = useState(false);
+  useEffect(()=>{
+    setLoading(true);
+    setTimeout(()=>{
+      setLoading(false)
+    },2000);
+  },[]);
   useEffect(() => {
-    Aos.init({duration:1000}); 
+    Aos.init({duration:500}); 
   }, [])
   // console.log(content);
   return (
-    <Router>
-      <div className="App">
-      <GlobalStyle/>
-      <Navbar/>
-      <ScrollToTop/> 
-          <Switch>
-            <Route exact path="/">
-              <div>
-                <Home Covertext={content[0]}/>
-              </div>
-            </Route>
-            <Route exact path="/destination">
-              <div>
-                <Destination Covertext={content[1]}/>
-              </div>
-            </Route>
-            <Route exact path="/blog">
-              <div>
-                <Blog Covertext={content[2]}/>
-              </div>
-            </Route>
-            <Route>
-              <div>
-                <AboutUs Covertext={content[3]}/>
-              </div>
-            </Route>
-          </Switch>
-          <Footer/>
-      </div>
-    </Router>
+    <div className='App'>
+        {
+          loading ?
+          <BeatLoader loading={loading} color={'#FFA000'} className="spinner"/>
+                  :
+                  <Router>
+                  <div className="App">
+                  <GlobalStyle/>
+                  <Navbar/>
+                  <ScrollToTop/> 
+                      <Switch>
+                        <Route exact path="/">
+                          <div>
+                            <Home Covertext={content[0]}/>
+                          </div>
+                        </Route>
+                        <Route exact path="/destination">
+                          <div>
+                            <Destination Covertext={content[1]}/>
+                          </div>
+                        </Route>
+                        <Route exact path="/blog">
+                          <div>
+                            <Blog Covertext={content[2]}/>
+                          </div>
+                        </Route>
+                        <Route>
+                          <div>
+                            <AboutUs Covertext={content[3]}/>
+                          </div>
+                        </Route>
+                      </Switch>
+                      <Footer/>
+                  </div>
+                </Router>
+         }
+    </div>
   );
 }
 
